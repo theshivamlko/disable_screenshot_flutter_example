d5ae3df96089ca0d4f1cf5b95836bf4551e5374f
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2022-03-31 00:46:34 +0530

    disable screenshot at app launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
   runApp(const MyApp());
 }
 
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Screenshot',
+      title: 'Enable/Disable Screenshot',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -29,53 +31,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool isEnabled = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black45,
-                  onPrimary: Colors.white,
-                  shadowColor: Colors.black45,
-                  elevation: 3,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32.0)),
-                  minimumSize: const Size(120, 50),
-                ),
-                onPressed: () async {
-                  await FlutterWindowManager.addFlags(
-                      FlutterWindowManager.FLAG_SECURE);
-                },
-                child: Text("Enable"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black45,
-                  onPrimary: Colors.white,
-                  shadowColor: Colors.black45,
-                  elevation: 3,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32.0)),
-                  minimumSize: const Size(120, 50),
-                ),
-                onPressed: () async {
-                  await FlutterWindowManager.clearFlags(
-                      FlutterWindowManager.FLAG_SECURE);
-                },
-                child: Text("Disable"),
-              ),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text("Disable taking screenshot"),
+      ),
+      body: SwitchListTile(
+        value: isEnabled,
+        title: const Text("Disable taking screenshot"),
+        onChanged: (value) async {
+          if (value) {
+            await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
+          } else {
+            await FlutterWindowManager.clearFlags(FlutterWindowManager.FLAG_SECURE);
+          }
+          setState(() {
+            isEnabled = value;
+          });
+        },
       ),
     );
   }
